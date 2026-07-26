438d5e988374596124e93941bd84f6d505a47795
aifpstop <aifpstop@126.com>
2026-07-27 00:01:49 +0800

    Add files via upload

---
+++ webconfigv2.js
@@ -0,0 +1,7 @@
+window.webconfig =
+{
+    "baseURL": "https://api.aifps.top",//http://localhost:8000  https://api.aifps.top
+    "timeout": 5000,
+    "learnURL":"https://space.bilibili.com/3706982515083678",
+    "withCredentials": true
+}
