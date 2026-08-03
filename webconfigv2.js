feabef227115d17403ecfb1e386cf59e393c0c08
aifpstop <aifpstop@126.com>
2026-08-03 08:52:48 +0800

    Update webconfigv2.js

--- webconfigv2.js
+++ webconfigv2.js
@@ -1,6 +1,6 @@
 window.webconfig =
 {
-    "baseURL": "https://api.aifps.top",//http://localhost:8000  https://api.aifps.top
+    "baseURL": "https://apispeed.aifps.top",  https://api.aifps.top
     "timeout": 5000,
     "learnURL":"https://space.bilibili.com/3706982515083678",
     "withCredentials": true
